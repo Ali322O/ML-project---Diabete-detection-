d97f31347f30b222bc0e154f5f7e311528a11f4f
Ali322O <aliamrjanil@gmail.com>
2025-11-05 22:51:38 +0100

    structure initale du projet

---
+++ src/ml_workflow.py
@@ -0,0 +1,14 @@
+# Description :
+#     Ce fichier contiendra toutes les fonctions du pipeline :
+#     - Nettoyage des données
+#     - Gestion des valeurs manquantes
+#     - Normalisation
+#     - Split train/test
+#     - Entraînement de plusieurs modèles
+#     - Sélection de variables
+#     - Évaluation des performances
+
+
+
+
+
---
+++ tests/test_preprocessing.py
@@ -0,0 +1,7 @@
+def f(n):
+    return n + 1
+def g(n):
+    return n * 2
+def h(n):
+    return n - 3
+
